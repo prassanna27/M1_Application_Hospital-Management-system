092055ca44b16f7f57600ffe0e36591f0f4a910e
prassanna27 <94218817+prassanna27@users.noreply.github.com>
2021-11-24 08:35:17 +0530

    Create project_main.c

---
+++ 3_Implementation/project_main.c
@@ -0,0 +1,33 @@
+#include<hms.h>
+char ans=0;
+int ok;
+int b, valid=0;
+
+struct patient//list of global variable
+{
+	int age;
+	char Gender;
+	char First_Name[20];
+	char Last_Name[20];
+	char Contact_no[15];
+	char Address[30];
+	char Email[30];
+	char Doctor[20];
+	char Problem[20];
+};
+
+struct patient  p,temp_c;
+
+
+
+main(void)
+{
+
+    WelcomeScreen();//Use to call WelcomeScreen function
+	Title();//Use to call Title function
+	LoginScreen();//Use to call Menu function
+
+
+
+
+}
